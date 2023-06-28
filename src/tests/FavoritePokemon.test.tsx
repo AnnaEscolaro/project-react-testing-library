7660429ee0e7f915dadb2814a5e5063c024d7d49
Anna Escolaro <annaescolaro@hotmail.com>
2023-06-28 15:10:47 -0300

    req 3

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,20 @@
-test('', () => {});
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('É exibida na tela a mensagem No favorite pokemon found caso a pessoa não tenha Pokémon favorito', () => {
+  renderWithRouter(<App />, { route: '/favorites' });
+  const noFavText = screen.getByText('No favorite Pokémon found');
+  expect(noFavText).toBeInTheDocument();
+});
+
+test('Apenas são exibidos os Pokémon favoritados', async () => {
+  renderWithRouter(<App />, { route: '/pokemon/25' });
+  const favoriteBox = screen.getByText('Pokémon favoritado?');
+  await userEvent.click(favoriteBox);
+  const favoritePage = screen.getByRole('link', { name: 'Favorite Pokémon' });
+  await userEvent.click(favoritePage);
+  const pokemonName = screen.getByRole('img', { name: 'Pikachu sprite' });
+  expect(pokemonName).toBeInTheDocument();
+});
