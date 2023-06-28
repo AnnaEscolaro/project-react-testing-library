4b5375d5a13d3d4cd2273c033614b82282077e75
Anna Escolaro <annaescolaro@hotmail.com>
2023-06-28 15:36:04 -0300

    req 4

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,15 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Se a página contém um heading h2 com o texto Page requested not found', () => {
+  renderWithRouter(<App />, { route: '/NotFound' });
+  const h2 = screen.getByText('Page requested not found');
+  expect(h2).toBeInTheDocument();
+});
+
+test('Se a página mostra a imagem', () => {
+  renderWithRouter(<App />, { route: '/NotFound' });
+  const image = screen.getByAltText('Pikachu crying because the page requested was not found');
+  expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
